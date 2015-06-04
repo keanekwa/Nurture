eb949f0ddf0f05f9d2d5a1eb275c9c5a275f25ff
jiexiong123 <jiexiong123@hotmail.com>
2015-06-04 19:53:50 +0800

    main_activity fixed

--- app/src/main/java/com/example/user/nurture/MainActivity.java
+++ app/src/main/java/com/example/user/nurture/MainActivity.java
@@ -20,7 +20,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
-import com.parse.ParseException;
 import com.parse.ParseUser;
 
 
@@ -40,17 +39,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-        checkLogin();
-        //mPlantie.setImageResource(imageIDs[0]);
-=======
-        //mPlantie.setImageResource(imageIDs[currentIndex]);
->>>>>>> origin/master
 
         mGiveButton = (Button) findViewById(R.id.GiveButton);
         mReceiveButton = (Button) findViewById(R.id.ReceiveButton);
         mPlantie = (ImageSwitcher)findViewById(R.id.Plant);
-        mMeter = (TextView) findViewById(R.id.Meter);
         mNewButton = (Button)findViewById(R.id.NewButton);
         mGiveButton.setOnClickListener(new View.OnClickListener() {
             @Override
