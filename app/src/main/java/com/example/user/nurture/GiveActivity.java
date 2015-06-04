d99f992667594cf8d38ace416e1755f19a029c16
Adriel AD Tan <ad55tan@gmail.com>
2015-06-04 12:31:43 +0800

    12:31 giveActivity done (haven't tested it yet though)

--- app/src/main/java/com/example/user/nurture/GiveActivity.java
+++ app/src/main/java/com/example/user/nurture/GiveActivity.java
@@ -1,17 +1,79 @@
 package com.example.user.nurture;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GiveActivity extends ActionBarActivity {
+    private ImageView mProfilePic;
+    private TextView mNameTextView;
+    private TextView mClassTextView;
+    private ArrayAdapter<String> mAdapter;
+    private ListView mListView;
+    private ArrayList<String> listOfKindness;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_give);
+
+        ////////////////////////
+        // set user "profile" //
+        ////////////////////////
+        mProfilePic = (ImageView)findViewById(R.id.profilePic);
+        mNameTextView = (TextView)findViewById(R.id.nameTextView);
+        mClassTextView = (TextView)findViewById(R.id.classTextView);
+
+        Intent intent = getIntent();
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("userInfo");
+        query.whereEqualTo("username", intent.getStringExtra("USERNAME"));
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, ParseException e) {
+                if(e==null && parseObjects.size()==1){
+                    ParseObject userInfo = parseObjects.get(0);
+                    mNameTextView.setText(userInfo.getString("username"));
+                    mClassTextView.setText(userInfo.getString("class"));
+                    //mProfilePic.setImageDrawable(userInfo.getFile("profilePic"));
+                }
+            }
+        });
+
+        //////////////////
+        // set listview //
+        //////////////////
+        mListView = (ListView)findViewById(android.R.id.list);
+        listOfKindness = new ArrayList<>();
+
+        ParseQuery<ParseObject> query1 = ParseQuery.getQuery("suggestedKindness");
+        query1.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, ParseException e) {
+                if(e==null) {
+                    for (ParseObject i : parseObjects) {
+                        listOfKindness.add(i.getString("kindness"));
+                    }
+                    mAdapter = new ArrayAdapter<>(GiveActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, listOfKindness);
+                    mListView.setAdapter(mAdapter);
+                }
+            }
+        });
     }
 
     @Override
