dcccd47b4256bb81b9a27756f096df47e58188fa
jiexiong123 <jiexiong123@hotmail.com>
2015-06-04 14:15:00 +0800

    imageswitcher added

--- app/src/main/java/com/example/user/nurture/LoginActivity.java
+++ app/src/main/java/com/example/user/nurture/LoginActivity.java
@@ -28,7 +28,7 @@ public class LoginActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getActionBar().hide();
+
 
         mSignUpTextView = (TextView) findViewById(R.id.signUpTextView);
         mSignUpTextView.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/user/nurture/NurtureApplication.java
+++ app/src/main/java/com/example/user/nurture/NurtureApplication.java
@@ -1,11 +1,12 @@
 package com.example.user.nurture;
 
 import android.app.Application;
-import android.os.Bundle;
+
 import com.parse.Parse;
 
 public class NurtureApplication extends Application {
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate() {
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "Zctf49We7TfLhWb68KJowNSBLYaSk84tSynUJPPE", "EUZqqBYGF9H4f88kZgLQ1TpJAlFrQ1j32tawxX5s");
