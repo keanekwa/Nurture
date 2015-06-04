48cd57307c0079b016fdf624d0a95aef8df0b6d0
keanekwa <keanekwa@live.com.sg>
2015-06-04 13:38:30 +0800

    1:38pm Fixed Parse Error

--- app/src/main/java/com/example/user/nurture/NurtureApplication.java
+++ app/src/main/java/com/example/user/nurture/NurtureApplication.java
@@ -1,11 +1,13 @@
 package com.example.user.nurture;
 
 import android.app.Application;
-import android.os.Bundle;
+
 import com.parse.Parse;
 
 public class NurtureApplication extends Application {
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate() {
+        super.onCreate();
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "Zctf49We7TfLhWb68KJowNSBLYaSk84tSynUJPPE", "EUZqqBYGF9H4f88kZgLQ1TpJAlFrQ1j32tawxX5s");
