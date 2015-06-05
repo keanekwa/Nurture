6bdb39cf3274d5127d77786b97a70bb857571a79
Jlprime <jameslao82@gmail.com>
2015-06-05 22:00:23 +0800

    1000

--- app/src/main/java/com/example/user/nurture/SignUpActivity.java
+++ app/src/main/java/com/example/user/nurture/SignUpActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -101,6 +100,7 @@ public class SignUpActivity extends Activity {
                                 newUserInfo.put("username", usernameInput);
                                 newUserInfo.put("hasDoneKindness", false);
                                 newUserInfo.put("school", schoolInput);
+                                newUserInfo.put("role", roleInput);
                                 newUserInfo.saveInBackground(new SaveCallback() {
                                     @Override
                                     public void done(ParseException e) {
