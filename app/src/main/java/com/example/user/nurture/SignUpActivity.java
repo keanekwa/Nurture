155e28404b3a1bfc87fe98a4898430752bdedf02
Jlprime <jameslao82@gmail.com>
2015-06-05 12:46:39 +0800

    1246 changed signupactivity

--- app/src/main/java/com/example/user/nurture/SignUpActivity.java
+++ app/src/main/java/com/example/user/nurture/SignUpActivity.java
@@ -1,23 +1,81 @@
 package com.example.user.nurture;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
 
 
 public class SignUpActivity extends ActionBarActivity {
+
+    EditText mUsernameBlank;
+    EditText mPasswordBlank;
+    EditText mCPasswordBlank;
+    EditText mEmailAddBlank;
+    String usernameInput;
+    String passwordInput;
+    String cPasswordInput;
+    String emailInput;
+    Button mAddAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().hide();
+
+
+        mUsernameBlank = (EditText) findViewById(R.id.UsernameBlank);
+        mPasswordBlank = (EditText) findViewById(R.id.PasswordBlank);
+        mCPasswordBlank = (EditText) findViewById(R.id.PasswordBlank2);
+        mEmailAddBlank = (EditText) findViewById(R.id.EmailBlank);
+
+        mAddAccount = (Button) findViewById(R.id.CreateButton);
+        mAddAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usernameInput = mUsernameBlank.getText().toString();
+                passwordInput = mPasswordBlank.getText().toString();
+                cPasswordInput = mCPasswordBlank.getText().toString();
+                emailInput = mEmailAddBlank.getText().toString();
+                if (usernameInput.equals("") | passwordInput.equals("") | cPasswordInput.equals("") | emailInput.equals("")){
+                    alertMessage("Please fill in the empty fields.");
+                    //checks for empty fields
+                }
+                else if (!passwordInput.equals(cPasswordInput)){
+                    alertMessage("Your passwords do not match. Please reenter.");
+                    mPasswordBlank.setText("");
+                    mCPasswordBlank.setText("");
+
+                }
+                else {
+                    ParseUser userObject = new ParseUser();
+                    userObject.setUsername(usernameInput);
+                    userObject.setPassword(passwordInput);
+                    userObject.setEmail(emailInput);
+                    userObject.signUpInBackground();
+                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                    SignUpActivity.this.startActivity(intent);
+                }
+            }});
+
 
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,5 +97,10 @@ public class SignUpActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void alertMessage(String Message)
+    {
+        Toast.makeText(SignUpActivity.this, Message, Toast.LENGTH_SHORT).show();
     }
 }
