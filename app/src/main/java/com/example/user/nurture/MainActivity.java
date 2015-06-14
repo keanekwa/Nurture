d06fc361911230821977b8a4d571e5dc1702c8ae
Adriel AD Tan <ad55tan@gmail.com>
2015-06-14 21:42:01 +0800

    9:41 useless blah
    blah

--- app/src/main/java/com/example/user/nurture/MainActivity.java
+++ app/src/main/java/com/example/user/nurture/MainActivity.java
@@ -224,7 +224,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is preqsent.
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
